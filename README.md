a7d6d4bd8f62c8e2c203c2c0be614489c0e3ee35
tiwarineha16 <70210210+tiwarineha16@users.noreply.github.com>
2024-06-15 19:05:58 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# LLM_APP
+## Using quantized LLM model on CPU with this set up
+
+
+# How to run ?
+
+1. Create a new environment
+
+conda create -n llmapp python=3.10 -y
+
+2. Activate the enviroment 
+
+conda activate llmapp
+
+3. Install the requirements
+
+pip install -r requirements.txt
+
+
---
+++ run_local.py
@@ -0,0 +1,56 @@
+import os
+import sys
+import locale
+from langchain import PromptTemplate
+from langchain import LLMChain
+from langchain.llms import CTransformers
+
+# Set the locale to UTF-8
+locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
+
+# Ensure stdout uses UTF-8 encoding
+sys.stdout.reconfigure(encoding='utf-8')
+
+
+B_INST, E_INST = "[INST]", "[/INST]"
+B_SYS, E_SYS = "<<SYS>>\n", "\n<</SYS>>\n\n"
+
+
+DEFAULT_SYSTEM_PROMPT="""\
+You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe. 
+Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. 
+Please ensure that your responses are socially unbiased and positive in nature.
+If a question does not make any sense, or is not factually coherent, explain why instead of 
+answering something not correct. If you don't know the answer to a question,
+please don't share false information."""
+
+
+instruction = "Convert the following text from English to Hindi: \n\n {text}"
+
+
+SYSTEM_PROMPT = B_SYS + DEFAULT_SYSTEM_PROMPT + E_SYS
+template = B_INST + SYSTEM_PROMPT + instruction + E_INST
+
+
+prompt = PromptTemplate(template= template, input_variables=['text'])
+
+
+llm = CTransformers(
+    model= "model/llama-2-7b-chat.ggmlv3.q4_0.bin",
+    model_type="llama",
+    config={
+        "max_new_tokens":128,
+        "temperature":0.01
+    }
+)
+
+
+llm_chain = LLMChain(
+    prompt = prompt,
+    llm = llm
+)
+
+
+response = llm_chain.run("what is your name?")
+
+print(response)
